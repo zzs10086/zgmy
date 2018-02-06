ce9f7c0c8e0142cd8bd387c95b22cdaee16eb441
zzs <563315963@qq.com>
2018-02-06 16:43:20 +0800

    init

---
+++ pages/index/index.js
@@ -0,0 +1,24 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    swrap: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+    var that = this;
+
+  },
+
+})
