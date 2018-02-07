64741d9e6c57e8ecd7476e4cebf7e9464f59b676
zzs <563315963@qq.com>
2018-02-07 17:39:01 +0800

    modify

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 //app.js
+var encryption = require('./utils/md5.js');
 App({
+  cacheKey:{
+
+    globCityInfo:'globCityInfo',
+
+    globLocationText:'globLocationText'
+
+  },
+
+  data_const: {
+
+    wechatley: 'zzs594ba@*2f5fH9zzs',
+
+    api_host: 'https://api.zhongguomayun.com',//上线的时候要改！！https://api.zhongguomayun.com
+
+    storage_key: {
+
+      user_info: 'user_info',
+
+    }
+
+  },
+
+
   onLaunch: function () {
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
@@ -35,5 +59,36 @@ App({
   },
   globalData: {
     userInfo: null
-  }
+  },
+
+  /**
+   * 封装wx.request：
+   * 1.取消api前缀及type=json
+   * 2.添加身份验证，params中auth参数为true时，将进行微信登录操作获取code传入服务端（参数为'_code'）
+   * 3.加密
+   * @param params
+   * @returns {*}
+   */
+  request: function(params) {
+    var that = this;
+    if (undefined == params.url) {
+      return false;
+    }
+    params.url = this.data_const.api_host + params.url + '?type=json';
+    params.method = "GET";
+    params.data.version = "1.1.0";
+    var time = ((new Date()).getTime()).toString();
+    var rand = (parseInt(Math.random()*(9999-1000+1) + 1000)).toString();
+    var token = encryption.md5(time + rand + this.data_const.wechatley);
+    params.data.time_wechat = time;
+    params.data.rand_wechat = rand;
+    params.data.token_wechat = token;
+    params.header = {"content-type": "application/x-www-form-urlencoded"};
+
+    return wx.request(params);
+  },
+
+ 
+
+
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -5,12 +5,15 @@ const app = getApp()
 Page({
   data: {
     motto: 'Hello World',
+    scrollTop: 0,
+    scrollHeight: 0,
     userInfo: {},
     hasUserInfo: false,
     imgUrls: [
       'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
       'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg'
-    ]
+    ],
+    newsList: {}
   },
   //事件处理函数
   bindViewTap: function() {
@@ -21,6 +24,54 @@ Page({
   onLoad: function () {
 
     var that = this;
+
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          scrollHeight: res.windowHeight
+        });
+      }
+    });
+    
+    var params = {};
+    params.page = 1;
+    params.category_id = 0;
+    params.limit = 10;
+
+    that._getNewsList(params);
+
+  },
+
+  _getNewsList:function (params) {
+
+    var that = this;
+    app.request({
+      url: '/api/feed',
+      data: params,
+      success: function (res) {
+
+        if (res.data.code == 0) {
+          console.log(res.data);
+          if (!res.data.data.length) { // 避免无效请求
+            that.setData({
+              invalid_data: true,
+              is_show: true,
+              hidden: true
+            })
+            return;
+          }
+
+          that.setData({
+            //newsList: that.data.newsList.concat(res.data.data),
+            newsList: res.data.data,
+            hidden: true
+          });
+
+
+        }
+      }
+
+    });
 
   },
 
